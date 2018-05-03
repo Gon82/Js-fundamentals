409506eecb90466e72ae8b103e15394de116ec79
gon82 <gon.williams@gmail.com>
2018-05-03 00:24:10 -0300

    ok

--- script.js
+++ script.js
@@ -17,28 +17,28 @@ var arr = [5,"a","b","c",1,3,2,4,6];
 
 	...................................................................*/   
 	
-	// function esPar(numeros){
-	//  if(numeros%2 == 0){
-	//      return true;
-	//  }
-	// }
-	// console.log( "son pares: " + arr.filter(esPar))  //-> son pares: 2,4,6
+	function esPar(numeros){
+	 if(numeros%2 == 0){
+	     return true;
+	 }
+	}
+	console.log( "son pares: " + arr.filter(esPar))  //-> son pares: 2,4,6
 	
 	
-	// //-//
+	//-//
 
 
-	// function buscarPares(array){
-	//  var listaDePares = array.filter(function(numeros){
-	//      if (numeros%2 == 0){
-	//          return true 
-	//      }
-	//  })
-	//  return listaDePares;
-	// }
+	function buscarPares(array){
+	 var listaDePares = array.filter(function(numeros){
+	     if (numeros%2 == 0){
+	         return true 
+	     }
+	 })
+	 return listaDePares;
+	}
 
-	// console.log(buscarPares(arr));   //-> [2, 4, 6]
-	// console.log(arr);    
+	console.log(buscarPares(arr));   //-> [2, 4, 6]
+	console.log(arr);    
 //e 
 //:::::::::::::::::::::::::::::  splice() | Changes the contents of an array by removing existing elements and/or adding new elements.
 
@@ -52,8 +52,8 @@ var arr = [5,"a","b","c",1,3,2,4,6];
   ...................................................................*/
   
 
-  // arr.splice(1,4,"NO","SI")
-  // console.log(arr); //-> [5, "NO", "SI", 3, 2, 4, 6]
+  arr.splice(1,4,"NO","SI")
+  console.log(arr); //-> [5, "NO", "SI", 3, 2, 4, 6]
 //e
 //:::::::::::::::::::::::::::::  slice()  | Returns a portion of an array into a new array selected from begin to end (end not included)
 
@@ -67,7 +67,7 @@ var arr = [5,"a","b","c",1,3,2,4,6];
    ...................................................................*/
 
   
-  // console.log(arr.slice(2,6));    //-> ["b", "c", 1, 3]
+  console.log(arr.slice(2,6));    //-> ["b", "c", 1, 3]
 //e  
 //:::::::::::::::::::::::::::::  concat() | Merge two or more arrays and returns a new array
   
@@ -79,16 +79,16 @@ var arr = [5,"a","b","c",1,3,2,4,6];
 
    ...................................................................*/
 
-  // var arr1 = ["3","2","1"]   
-  // var arr2 = ["si","no"]   
-  // var arr3 = ["hola", "chau"]  
+  var arr1 = ["3","2","1"]   
+  var arr2 = ["si","no"]   
+  var arr3 = ["hola", "chau"]  
 
-  // console.log(arr1.concat(arr2,arr3));    //->  ["3", "2", "1", "si", "no", "hola", "chau"]
+  console.log(arr1.concat(arr2,arr3));    //->  ["3", "2", "1", "si", "no", "hola", "chau"]
 
-  // //-//
+  //-//
 
-  // var arr4 = arr1.concat(arr2,arr3);
-  // console.log(arr4);
+  var arr4 = arr1.concat(arr2,arr3);
+  console.log(arr4);
 
 //e  
 //:::::::::::::::::::::::::::::  push()   | Adds one or more elements to the end of an array and returns the new length of the array
@@ -100,11 +100,11 @@ var arr = [5,"a","b","c",1,3,2,4,6];
    ...................................................................*/
 
 
-  // var arrPushy = [1,2,3] 
+  var arrPushy = [1,2,3] 
 
-  // console.log(arrPushy); //-> [1,2,3] 
-  // console.log(arrPushy.push("culo")); //-> 4 
-  // console.log(arrPushy); //-> [1,2,3,"culo"] 
+  console.log(arrPushy); //-> [1,2,3] 
+  console.log(arrPushy.push("culo")); //-> 4 
+  console.log(arrPushy); //-> [1,2,3,"culo"] 
 
 //e  
 //:::::::::::::::::::::::::::::  map()    | Creates a new array with the results of calling a provided function on every element in the calling array.
@@ -123,13 +123,28 @@ var arr = [5,"a","b","c",1,3,2,4,6];
 	#   No modifica array original
 
    ...................................................................*/
-  //  		var arrMap = [1,2,3]
+   		var arrMap = [1,2,3]
    		
-  //  		function exclama(array){
-  //   		return array + "!";
-		// };
+   		function exclama(array){
+    		return array + "!";
+		};
    		
-		// var arrMapped= arrMap.map(exclama)
-		// console.log(arrMapped); 			//-> ["1!", "2!", "3!"]
-		// console.log(arrMap); 			 	//-> ["1", "2", "3"]
+		var arrMapped= arrMap.map(exclama)
+		console.log(arrMapped); 			//-> ["1!", "2!", "3!"]
+		console.log(arrMap); 			 	//-> ["1", "2", "3"]
+//e 
+
+//:::::::::::::::::::::::::::::  Sort()    | Creates a new array with the results of calling a provided function on every element in the calling array.
+
+/*...................................................................
+ 
+#   var new_array = arr.map(function callback(currentValue[, index[, array]]) {
+			// Return element for new_array
+	}[, thisArg])
+
+
+ ...................................................................*/
+var arrMap = [1, 2, 3]
+
+
 //e 
